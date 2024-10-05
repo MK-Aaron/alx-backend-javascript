401ccf6b3aa46285ba66a166c1d678e3b6660c6e
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-10-05 04:36:21 +0300

    COmpleted mandatory task

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((response) => {
+      const [photoResponse, userResponse] = response;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
